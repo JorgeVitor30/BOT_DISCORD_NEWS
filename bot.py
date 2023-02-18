c0db80ded8162afc236351bea9a056331586cca0
Pedro Coutinho <pedro.coutinho@fortbrasil.com.br>
2023-02-18 20:32:38 -0300

    Envio de noticias configurado corretamente(sem repetição de noticias)

--- bot.py
+++ bot.py
@@ -1,30 +1,20 @@
 import discord
-import os
 from discord.ext import commands, tasks
 from buscar_dados_noticia import *
-import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+lista = []
 
 bot = commands.Bot("#", intents = intents)
 
 Buscar = BuscarDados('https://www.cnnbrasil.com.br/ultimas-noticias/')
 
 link = Buscar.buscar_new()
-
+i = 0
 # FAZER UMA LISTA COM OS ULTIMOS LINKS PRA FZR UM HISTORICO DOS ULTIMOS 5
 # FAZER UMA VERIFICAÇÃO DA LISTA ATUAL DENTRO DO ARRAY
-
-
-# async def on_message(self, message):
-#     if "#função" in message.content:
-#         await message.channel.send("DAR NOTICIAS")
-            
-#     if "#abc" in message.content:
-#         pass
-            
- 
 @bot.listen()
 async def on_ready():
     Variavel.start() 
@@ -32,12 +22,21 @@ async def on_ready():
     
 @tasks.loop(seconds=5)
 async def Variavel():
+    global i
+    global link 
+
+    use_link = Buscar.buscar_new()
     channel = bot.get_channel(1074361880145043617)
-    await channel.send(link)
+    
+    if use_link != link:
+        link = use_link
+        await channel.send(link)
+    elif i == 0:
+        i += 1
+        await channel.send(link)
+    else:
+        return
         
-
-
-
 
 
 bot.run('MTA3NTIxNDQxODM0MzkwNzQyOA.G4qd3R.pvBNYpPZbS4KaoQVfflkgzsRnFK6iLEy6Yr7Jo')

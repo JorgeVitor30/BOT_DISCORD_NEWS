007ee6baab18d4a57b95cb9644f0ba4e224de5be
Jorge Vitor <103287884+JorgeVitor30@users.noreply.github.com>
2023-02-22 02:25:51 -0300

    Otimizando os GETS e Personalizando o BOT

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from buscar_dados_noticia import *
 intents = discord.Intents.default()
 intents.message_content = True
 
-lista = []
+token = 'MTA3NTIxNDQxODM0MzkwNzQyOA.G4qd3R.pvBNYpPZbS4KaoQVfflkgzsRnFK6iLEy6Yr7Jo'
 
 bot = commands.Bot("#", intents = intents)
 
@@ -13,32 +13,35 @@ Buscar = BuscarDados('https://www.cnnbrasil.com.br/ultimas-noticias/')
 
 link = Buscar.buscar_new()
 i = 0
-# FAZER UMA LISTA COM OS ULTIMOS LINKS PRA FZR UM HISTORICO DOS ULTIMOS 5
-# FAZER UMA VERIFICAÇÃO DA LISTA ATUAL DENTRO DO ARRAY
+
 @bot.listen()
 async def on_ready():
-    Variavel.start() 
+    envio_noticias.start() 
     
     
 @tasks.loop(seconds=5)
-async def Variavel():
+async def envio_noticias():
     global i
     global link 
+    channel_id = 1074361880145043617
 
     use_link = Buscar.buscar_new()
-    channel = bot.get_channel(1074361880145043617)
+    channel = bot.get_channel(channel_id)
     
     if use_link != link:
         link = use_link
+        await channel.send("MAIS UMA NOTÍCIA 🤮")
         await channel.send(link)
-    elif i == 0:
+        await channel.send("@everyone")
+
+    elif i == 0:    
         i += 1
+        await channel.send("MAIS UMA NOTÍCIA 🤮")
         await channel.send(link)
+        await channel.send("@everyone")
     else:
         return
         
 
 
-bot.run('MTA3NTIxNDQxODM0MzkwNzQyOA.G4qd3R.pvBNYpPZbS4KaoQVfflkgzsRnFK6iLEy6Yr7Jo')
-
-
+bot.run(token)
